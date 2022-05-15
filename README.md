a179b746521d7e5bd361d229049b70eb0eceb16f
jasmine <jasminevirk10@gmail.com>
2022-05-15 14:29:01 -0400

    final commit

---
+++ README.md
@@ -0,0 +1,43 @@
+Description
+
+A node.js application that uses inquirer to prompt different questions to create readme file.
+
+Table of Contents
+
+Description
+Installation
+Usage
+License
+Contributing
+Tests
+Questions
+Installation
+
+Two packages are installed :
+1. npm init 
+2. npm install inquirer
+
+Usage
+
+This will create readme file for users.
+node index.js
+
+License
+
+badge This application is covered by the MIT license.
+
+For more information on license types, please reference to this website for additional licensing information - LICENSE INFORMATION.
+
+Contributing
+jasmine virk
+Tests
+
+Validation tests and quality assurance improved the overall file.
+
+Questions?
+
+Feel free to contact for questions:
+
+Contact me on Github:https://github.com/jvirk10
+
+Email for questions:jasminevirk10@gmail.com
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,77 @@
+function renderLicenseBadge(license) {
+    console.log(license);
+    if (license === "No License") {
+        return ``;
+    } else {
+        return `[![badge](https://img.shields.io/badge/license-${license}-brightgreen)](./LICENSE)`
+    }
+}
+
+function renderLicenseLink(license) {
+    if (license === "NO License") {
+        return ``;
+    } else {
+        return `for additional licensing information - [LICENSE INFORMATION](https://opensource.org/licenses).`
+    }
+}
+
+function renderLicenseSection(license) {
+    if (license === "No License") {
+        return `No License`;
+    } else {
+        return `${renderLicenseBadge(license)}
+        This application is covered by the ${license} license.
+        
+        For more information on license types, please reference to this website ${ renderLicenseLink(license)}`
+    }
+}
+
+function generateMarkdown(answers) {
+    console.log(answers);
+    const { title, description, installation, usage, license, contributing, tests, questions, username, email } = answers;
+    return `
+        # ${title} ${renderLicenseBadge(license)}
+        
+        ## Description
+        
+        ${description}
+        
+        ## Table of Contents
+        - [Description](#description)
+        - [Installation](#installation)
+        - [Usage](#usage)
+        - [License](#license)
+        - [Contributing](#contributing)
+        - [Tests](#tests)
+        - [Questions](#questions)
+        
+        ## Installation
+        
+        ${installation}
+        
+        ## Usage
+        
+        ${usage}
+        
+        ## License
+        
+        ${renderLicenseSection(license)}
+        
+        ## Contributing
+        
+        ${contributing}
+        
+        ## Tests
+        
+        ${tests}
+        
+        ## Questions?
+        
+        ${questions}
+        * Find me on Github:[${username}](https://github.com/${username})
+        * Email me with any questions: [${email}](mailto:${email})
+        
+        `;
+}
+
+module.exports = generateMarkdown;
